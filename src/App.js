6c679c4ef980712f2291db70dfba97f4dd4f5d90
veryharsh123 <76126983+veryharsh123@users.noreply.github.com>
2024-02-23 08:28:37 +0530

    Installation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home"
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import ForgotPassword from './pages/ForgotPassword';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Offers' element={< Offers/>} />
+        <Route path='/Profile' element={<Profile />} />
+        <Route path='/SignUp' element={<SignUp />} />
+        <Route path='/SignIn' element={< SignIn/>} />
+        <Route path='/ForgotPassword' element={< ForgotPassword/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 

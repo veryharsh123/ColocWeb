9642ea738450458847bb2a3acec999c689483b76
veryharsh123 <76126983+veryharsh123@users.noreply.github.com>
2024-04-01 02:46:28 +0530

    added features

--- src/pages/ListingItem.jsx
+++ src/pages/ListingItem.jsx
@@ -1,6 +1,32 @@
+import Moment from "react-moment"
+import { MdLocationOn } from "react-icons/md";
+import { Link } from "react-router-dom"
 export default function ListingItem({listing,id}) {
   return (
-    <div>{listing.name}
-    </div>
+   <li className="relative bg-gray-900 flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden m-[10px]">
+    <Link className="contents" to={`/category/${listing.type}/${id}`}>
+        <img className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in" loading="lazy" src={listing.imgUrls[0]} alt="Helll nawww"/>
+        <Moment fromNow className="absolute top-1 left-1 bg-blue-800 rounded-md px-2 py-1 shadow-lg">
+            {listing.timestamp?.toDate()}
+        </Moment>
+     <div className="w-full p-[10px]">
+        <div className="flex items-center space-x-1">
+            <MdLocationOn className="h-4 w-4"/>
+            <p className="font-semibold text-sm mb-[2px] truncate">{listing.address}</p>
+        </div>
+        <p className="font-semibold mt-2 text-xl truncate">{listing.name}</p>
+        <p className="text-blue-600 mt-2 font-semibold">₹{listing.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{listing.type === "rent" && " /Month"}</p>
+        <div className="flex space-x-6 items-center mt-2">
+            <div >
+                <p className="font-bold text-xs">{listing.bedroom > 1 ? `${listing.bedroom} Beds` : `${listing.bedroom} Bed`}</p>
+            </div>
+            <div>
+            <p className="font-bold text-xs">{listing.bathroom > 1 ? `${listing.bathroom} Baths` : `${listing.bathroom} Bath`}</p>
+            </div>
+        </div>
+     </div>
+    </Link>
+    </li>
+   
   )
 }

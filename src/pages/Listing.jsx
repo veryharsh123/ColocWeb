793581db8c82aff6a11fe6bcdfe121c10d2f998e
veryharsh123 <76126983+veryharsh123@users.noreply.github.com>
2024-04-13 02:27:29 +0530

    Maps Added

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -10,6 +10,7 @@ import { FaBed } from "react-icons/fa6";
 import { FaBath } from "react-icons/fa6";
 import Spinner from '../components/spinner';
 import { MdIosShare } from "react-icons/md";
+import {TileLayer, MapContainer, Marker, Popup} from "react-leaflet";
 import { FaMapMarkerAlt, FaParking, FaChair } from "react-icons/fa";
 import Contact from '../components/Contact';
 export default function Listing() {
@@ -98,7 +99,19 @@ export default function Listing() {
         </div>)}
         {contactOnwer && (<Contact userRef={listing.userRef} listing={listing}/>)}
         </div>
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+        <MapContainer center={[listing.geolocation.lat,listing.geolocation.lng]} zoom={13} scrollWheelZoom={false} style={{height:"100%", width:"100%"}}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[listing.geolocation.lat,listing.geolocation.lng]}>
+      <Popup>
+      {listing.address}
+      </Popup>
+    </Marker>
+  </MapContainer>
+        </div>
       </div>
 </main>
 )}

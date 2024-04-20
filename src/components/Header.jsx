42b7ea1a7a31968cf59515ad88075ce0b5b78508
veryharsh123 <76126983+veryharsh123@users.noreply.github.com>
2024-04-20 13:09:54 +0530

    minor fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,11 +22,11 @@ function pathMatch(route){
 }
   return (
     <div id="divv" className='bg-black hover:bg-white hover:text-blue-950 transition ease-in-out duration-150 shadow-sm sticky top-0 z-50'>
-      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
-        <div>
-        <img src="https://iili.io/JGxhJIf.png" alt="JGxhJIf.png" border="0" className='h-10 cursor-pointer' onClick={()=>navigate("/")}/>
-        </div>
-        <div>
+      <header className='navbarNav flex justify-between items-center px-3 max-w-6xl mx-auto'>
+      <div>
+        <img src="https://iili.io/JGxhJIf.png" alt="JGxhJIf.png" border="0" className='lg:h-10 h-7 cursor-pointer' onClick={()=>navigate("/")}/>
+      </div>
+      <div>
        <ul className='flex space-x-10'>
         <li className={`cursor-ponter py-3  text-sm font-semibold border-b-[3px] cursor-pointer text-blue-600 ${pathMatch("/") && "border-b-blue-500 "}`} onClick={()=>navigate("/")}>Home</li>
         <li className={`cursor-ponter py-3  text-sm font-semibold border-b-[3px] cursor-pointer text-blue-600 ${pathMatch("/find") && "border-b-blue-500 "}`} onClick={()=>navigate("/find")}>Find Flatmate</li>
